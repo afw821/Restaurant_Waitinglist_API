ac81a6a8ce53ff59056f937fb590dfeaddba93bf
afw821 <afw821@gmail.com>
2019-01-08 13:53:58 -0500

    I added the data to the table list and waitling list

---
+++ data/table-list.js
@@ -0,0 +1,10 @@
+const tableArray = [
+    {
+        customerName: "Alex Watkins",
+        customerEmail: "afw821@gmail.com",
+        customerPhone: "770-777-7777",
+        customerId: "1234"
+    }
+]
+
+module.exports = tableArray;
